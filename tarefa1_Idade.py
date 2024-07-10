a820c77c8d9568ab2fdf8b2c2186624c5984a48f
Hector <80215735+8ector@users.noreply.github.com>
2024-07-10 12:20:06 +0100

    Update tarefa1_Idade.py
    Versão 2.0

--- tarefa1_Idade.py
+++ tarefa1_Idade.py
@@ -1,21 +1,45 @@
 from datetime import datetime
 
+def obter_data_nascimento():
+    while True: 
+        try:
+            data_nascimento = int(input("Digite seu ano de nascimento (formato AAAA) "))
+            return data_nascimento
+        except ValueError:
+            print("Data inválida. Por favor, digite no formato AAAA")
 
 
-while True:
-    try:
-        idade = int(input("Qual é a sua idade?"))
-        break
-    except ValueError:
-        print("Dados incorreto")
+nome = input("Digite seu nome: ")
+data_nascimento = obter_data_nascimento()
+pessoa = {
+    'nome': nome,
+    'data_nascimento': data_nascimento
+}
+
+print(f"\nNome: {pessoa['nome']}, Ano de Nascimento: {pessoa['data_nascimento']}")
+
+data_atual = datetime.now()
+ano_atual = data_atual.year
+print(f"Estamos no ano de: {ano_atual}\n")
+idade = ano_atual - data_nascimento
+print(f"Idade: {idade} anos")
 
 
-if idade >= 18 and idade <= 125:
-    print("Maior de 18")
+if (idade <= 10) and (idade >= 0):
+    print("Criança")
+
+elif 10 < idade <= 18:
+    print("Adolescente")
+
+elif 18 < idade <= 65:
+    print("Adulto")
+
+elif 65 < idade <= 125:
+    print("Sénior")    
 
 elif idade > 125:
-    print("Idade muita elevada, já és uma múmia")
+    print("Múmia")   
 
-else:
-    print("Idade deve ser maior do que zero!!")
+elif idade < 0:
+    print("Ainda não Nasceu")   
 
